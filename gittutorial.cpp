5a97a912d27762a8ccb9df56896b2ce4fee4510d
Dawid Podziewski <dawid.podziewski@globallogic.com>
2024-04-18 22:16:04 +0200

    update z domu dellpraca

--- gittutorial.cpp
+++ gittutorial.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+<<<<<<< HEAD
     std::cout<<"Oks!";
 	std::cout<<"Dopis!";
+=======
+    std::cout<<"Oks!"; // kom z brench kwiecien , 2 zmiana
+    std::cout<<"Oks222!";
+
+>>>>>>> 5c7aed43250474e94e6d0cf6e68c62fce28a0c51
 }
